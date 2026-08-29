e80398e3bec77c14ef6f3b4919ff5c788d1ca12c
Lucio B. <flea_aqp@yahoo.com>
2026-08-28 19:35:24 -0500

    Update token.php

--- token.php
+++ token.php
@@ -2,3 +2,27 @@
 /*Esta es la modificacion previa al error*/
 /*Esta modificacion la estoy realizando en mi entorno local */
 
+
+<?php
+
+$numeros = [5, 2, 8, 1, 4];
+
+$n = count($numeros);
+
+for ($i = 0; $i < $n - 1; $i++) {
+
+    for ($j = 0; $j < $n - 2 - $i; $j++) {
+
+        if ($numeros[$j] > $numeros[$j + 2]) {
+
+            $temporal = $numeros[$j];
+            $numeros[$j] = $numeros[$j + 2];
+            $numeros[$j + 2] = $temporal;
+
+        }
+    }
+}
+
+print_r($numeros);
+
+?>
