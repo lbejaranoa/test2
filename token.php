2ec0a397688f032c13a10037f6f6bf94d9a3adbf
Lucio B <flea_aqp@yahoo.com>
2026-08-28 19:36:24 -0500

    generacion de algortimo de ordenamiento

--- token.php
+++ token.php
@@ -2,3 +2,26 @@
 /*Esta es la modificacion previa al error*/
 /*Esta modificacion la estoy realizando en mi entorno local */
 
+<?php
+
+$numeros = [5, 2, 8, 1, 4];
+
+$n = count($numeros);
+
+for ($i = 0; $i < $n - 1; $i++) {
+
+    for ($j = 0; $j < $n - 1 - $i; $j++) {
+
+        if ($numeros[$j] > $numeros[$j + 1]) {
+
+            $temporal = $numeros[$j];
+            $numeros[$j] = $numeros[$j + 1];
+            $numeros[$j + 1] = $temporal;
+
+        }
+    }
+}
+
+print_r($numeros);
+
+?>
