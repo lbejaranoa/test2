61c636c75d78e9eb854d83711dbc1f215d6b5bc1
Lucio B. <flea_aqp@yahoo.com>
2026-09-08 19:14:12 -0500

    PedidoService desarrollado
    Add method to register an order with IGV calculation and output.

--- PedidoServise.java
+++ PedidoServise.java
@@ -1,4 +1,19 @@
 public class PedidoService {
 
-   
+    public void registrarPedido(String cliente, double total) {
+
+        double igv = total * 0.18;
+        double totalConIgv = total + igv;
+
+        System.out.println("Pedido registrado");
+        System.out.println("Cliente: " + cliente);
+        System.out.println("Subtotal: S/ " + total);
+        System.out.println("IGV: S/ " + igv);
+        System.out.println("Total: S/ " + totalConIgv);
+    }
+
+    public static void main(String[] args) {
+        PedidoService service = new PedidoService();
+        service.registrarPedido("Empresa ABC", 1500);
+    }
 }
