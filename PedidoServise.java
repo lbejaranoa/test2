a819f1539e2f5a6114c8f6278e68e8f86931d926
Lucio B <flea_aqp@yahoo.com>
2026-09-08 19:13:30 -0500

    Pedido Service desarrollado

--- PedidoServise.java
+++ PedidoServise.java
@@ -1,4 +1,19 @@
 public class PedidoService {
 
-   
+    public void registrarPedido(String cliente, double total) {
+
+        if (total <= 0) {
+            System.out.println("Error: el total debe ser mayor a cero.");
+            return;
+        }
+
+        System.out.println("Pedido registrado correctamente");
+        System.out.println("Cliente: " + cliente);
+        System.out.println("Total: S/ " + total);
+    }
+
+    public static void main(String[] args) {
+        PedidoService service = new PedidoService();
+        service.registrarPedido("Empresa ABC", 1500);
+    }
 }
